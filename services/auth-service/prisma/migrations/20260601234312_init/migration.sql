a16826223d1035275e87db6fe373f9b6bd2c068f
DaviMoraisdev <davimorais063@gmail.com>
2026-06-01 21:10:43 -0300

    feat: adicionar Prisma com schema de usuario e migration inicial

---
+++ services/auth-service/prisma/migrations/20260601234312_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'SELLER', 'BUYER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BUYER',
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
